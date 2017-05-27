2b6ff960a90be9d80dcf9ac143f5b295f48d5f38
Zoltán T. Szeredi <zoltan.szeredi@3pillarglobal.com>
2017-05-27 12:44:55 +0300

    Some progress, not much...

--- app/main.js
+++ app/main.js
@@ -1,25 +1,91 @@
-var Game = function() {
+(function () {
+    var game = new Phaser.Game(
+        window.innerWidth, window.innerHeight, Phaser.AUTO, 'tankr',
+        { preload: preload,
+          create: create,
+          update: update }
+    );
+    var map;
+    var player;
+    var cursors;
 
-    this._width = window.innerWidth;
-    this._height = window.innerHeight;
-
-    this.game = new Phaser.Game(this._width, this._height, Phaser.AUTO, '', {
-        preload: this.preload,
-        create: this.create,
-        update: this.update
-    });
-}
-
-Game.prototype = {
-    preload: function() {
-
-    },
-    create: function() {
-
-    },
-    update: function() {
-
+    function preload() {
+        console.log('preload');
+        // game.load.tilemap('tankr', 'assets/map/tanker-14x14.json', null,
+        //                   Phaser.Tilemap.TILED_JSON);
+        game.load.image('tankBlue', 'assets/img/Tanks/tankBlue.png');
+        game.load.image('barrelBlue', 'assets/img/Tanks/barrelBlue.png');
+        // game.load.image('earth', 'assets/map/tanker-14x14.png');
     }
-}
 
-new Game();
+    function create() {
+        console.log('create');
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        player = game.add.sprite(0, 0, 'tankBlue');
+        // player.anchor.setTo(0.5, 0.5);
+        barrel = game.add.sprite(0, 0, 'barrelBlue');
+        // barrel.anchor.setTo(0.3, 0.5);
+        game.physics.arcade.enable(player);
+        game.camera.follow(player);
+        game.camera.focusOnXY(0, 0);
+        // map = game.add.tilemap('tankr');
+        // map = this.game.add.tilemap('MyTilemap');
+        // map.addTilesetImage('tiles', 'tiles');
+
+        // layer = map.createLayer('MyTerrain');
+        // layer.resizeWorld();
+        // layer.wrap = true;
+
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+    function update() {
+        console.log('update');
+        var units = 50,
+            angle = 3;
+
+        if (cursors.left.isDown) {
+            // player.body.velocity.x += -units;
+            player.angle += -angle;
+            player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            // player.body.velocity.x += units;
+            player.angle += angle;
+            player.animations.play('right');
+        } else if (cursors.down.isDown) {
+            player.body.velocity.y += units;
+            player.animations.play('down');
+        } else if (cursors.up.isDown) {
+            player.body.velocity.y += -units;
+            player.animations.play('up');
+        } else {
+            player.animations.stop();
+            player.frame = 4;
+        }
+        if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+        {
+            player.body.velocity.y = -350;
+        }
+    }
+
+    function render() {
+        console.log('render');
+    }
+
+    // PlayerTank = function (game) {
+    //     this.game = game;
+    //     this.tank = game.add.sprite(
+    //         game.world.randomX, game.world.randomY,
+    //         'tank', 'tankRed'
+    //     );
+
+    //     game.physics.enable(this.tank, Phaser.Physics.ARCADE);
+    //     this.tank.angle = game.rnd.angle();
+    //     game.physics.arcade.velocityFromRotation(
+    //         this.tank.rotation,
+    //         100,
+    //         this.tank.body.velocity
+    //     );
+    // };
+
+}());
