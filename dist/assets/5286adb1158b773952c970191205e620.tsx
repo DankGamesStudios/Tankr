18d87255e5c4c5c93f453ff55ae8a72990fc44d7
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2018-03-17 11:36:28 +0200

    add temp assets for live demo

---
+++ dist/assets/5286adb1158b773952c970191205e620.tsx
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bullets" tilewidth="20" tileheight="34" tilecount="24" columns="0">
+ <tile id="0">
+  <image width="12" height="26" source="../img/Bullets/bulletBeige.png"/>
+ </tile>
+ <tile id="1">
+  <image width="20" height="34" source="../img/Bullets/bulletBeige_outline.png"/>
+ </tile>
+ <tile id="2">
+  <image width="12" height="26" source="../img/Bullets/bulletBeigeSilver.png"/>
+ </tile>
+ <tile id="3">
+  <image width="20" height="34" source="../img/Bullets/bulletBeigeSilver_outline.png"/>
+ </tile>
+ <tile id="4">
+  <image width="12" height="26" source="../img/Bullets/bulletBlue.png"/>
+ </tile>
+ <tile id="5">
+  <image width="20" height="34" source="../img/Bullets/bulletBlue_outline.png"/>
+ </tile>
+ <tile id="6">
+  <image width="12" height="26" source="../img/Bullets/bulletBlueSilver.png"/>
+ </tile>
+ <tile id="7">
+  <image width="20" height="34" source="../img/Bullets/bulletBlueSilver_outline.png"/>
+ </tile>
+ <tile id="8">
+  <image width="12" height="26" source="../img/Bullets/bulletGreen.png"/>
+ </tile>
+ <tile id="9">
+  <image width="20" height="34" source="../img/Bullets/bulletGreen_outline.png"/>
+ </tile>
+ <tile id="10">
+  <image width="12" height="26" source="../img/Bullets/bulletGreenSilver.png"/>
+ </tile>
+ <tile id="11">
+  <image width="20" height="34" source="../img/Bullets/bulletGreenSilver_outline.png"/>
+ </tile>
+ <tile id="12">
+  <image width="12" height="26" source="../img/Bullets/bulletRed.png"/>
+ </tile>
+ <tile id="13">
+  <image width="20" height="34" source="../img/Bullets/bulletRed_outline.png"/>
+ </tile>
+ <tile id="14">
+  <image width="12" height="26" source="../img/Bullets/bulletRedSilver.png"/>
+ </tile>
+ <tile id="15">
+  <image width="20" height="34" source="../img/Bullets/bulletRedSilver_outline.png"/>
+ </tile>
+ <tile id="16">
+  <image width="12" height="26" source="../img/Bullets/bulletSilver.png"/>
+ </tile>
+ <tile id="17">
+  <image width="20" height="34" source="../img/Bullets/bulletSilver_outline.png"/>
+ </tile>
+ <tile id="18">
+  <image width="12" height="26" source="../img/Bullets/bulletSilverSilver.png"/>
+ </tile>
+ <tile id="19">
+  <image width="20" height="34" source="../img/Bullets/bulletSilverSilver_outline.png"/>
+ </tile>
+ <tile id="20">
+  <image width="12" height="26" source="../img/Bullets/bulletYellow.png"/>
+ </tile>
+ <tile id="21">
+  <image width="20" height="34" source="../img/Bullets/bulletYellow_outline.png"/>
+ </tile>
+ <tile id="22">
+  <image width="12" height="26" source="../img/Bullets/bulletYellowSilver.png"/>
+ </tile>
+ <tile id="23">
+  <image width="20" height="34" source="../img/Bullets/bulletYellowSilver_outline.png"/>
+ </tile>
+</tileset>
