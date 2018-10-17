b1336a5845eb38bd37d7266357c1f695de6c2a1a
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2018-10-17 22:11:42 +0300

    Update deploy script

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir dist
 
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
-
 git remote remove origin
 git remote add origin "https://${GITHUB_TOKEN}@github.com:DankGamesStudios/Tankr.git"
+
 npm install
+npm run-script build:dist
 npm run-script deploy
