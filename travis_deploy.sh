b0e9ca133be925b3f7d778c99569d2a2764b3c16
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2018-10-17 22:23:07 +0300

    Fix deploy origin url

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -3,7 +3,7 @@
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 git remote remove origin
-git remote add origin "https://${GITHUB_TOKEN}@github.com:DankGamesStudios/Tankr.git"
+git remote add origin "https://${GITHUB_TOKEN}@github.com/DankGamesStudios/Tankr.git"
 
 npm install
 npm run-script build:dist
